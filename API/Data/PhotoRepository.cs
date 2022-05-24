42bd57cada56befa70648fb3170d8ae6871c64be
LebohangMakateng <tiisetsomakateng@gmail.com>
2022-05-24 13:53:40 +0200

    PhotoRepositoryAdded(prior method bodies)

--- API/Data/PhotoRepository.cs
+++ API/Data/PhotoRepository.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
 namespace API.Data
 {
-    public class PhotoRepository
+    public class PhotoRepository : IPhotoRepository
     {
-        
+        private readonly DataContext _context;
+        public PhotoRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Photo> GetPhotoById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<PhotoForApprovalDto>> GetUnapprovedPhotos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePhoto(Photo photo)
+        {
+            _context.Photos.Remove(photo);
+        }
     }
 }
